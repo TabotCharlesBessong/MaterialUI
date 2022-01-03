8e166524d9e952b94e7027ab7c83cd07912bdef7
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-03 08:38:00 +0100

    missing packages and files

--- LoginAndRegistration/src/components/Header.js
+++ LoginAndRegistration/src/components/Header.js
@@ -1,88 +1,63 @@
-
-
-import { AppBar, Badge, Grid, IconButton, InputBase, Toolbar } from '@material-ui/core'
-import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew"
 import React from 'react'
-import NotficationNoneIcon from "@material-ui/icons/NotificationsNone"
-import SearchIcon from "@material-ui/icons/Search"
-import { makeStyles } from '@material-ui/styles'
+import { AppBar, Toolbar, Grid, InputBase, IconButton, Badge, makeStyles } from '@material-ui/core'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
 
 
-const useStyles = makeStyles(theme =>({
-  root:{
-    background:'#1ba785',
-    float:'right',
-  },
-  header1:{
-    position:'absolute',
-    top:'0.6rem',
-    right:'1rem'
-  },
-  header2:{
-    background:'#fff',
-    color:"#222",
-    width:'100%',
-    borderRadius:'0.75rem',
-    height:'3rem',
-  },
-  input:{
-    opacity:'0.6',
-    fontSize:'0.8rem',
-    padding:'0 0.8rem',
-    width:'100%',
-    height:'100%',
-    fontSize:'2rem',
-    letterSpacing:'0.1rem',
-    textTransform:'capitalize',
-    '&:hover':{
-      background:'#222',
-      color:'#fff'
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: '#fff',
+        
     },
-    '& .MuiSvgIcon-root':{
-      marginRight:'1rem',
+    searchInput: {
+        opacity: '0.6',
+        padding: `0px ${theme.spacing(1)}px`,
+        fontSize: '0.8rem',
+        '&:hover': {
+            backgroundColor: '#f2f2f2'
+        },
+        '& .MuiSvgIcon-root': {
+            marginRight: theme.spacing(1)
+        }
     }
-  },
 }))
 
-const Header = () => {
-  const classes = useStyles()
-  return (
-    <div className='header'>
-      <AppBar position='ststic' className={classes.root}>
-        <Toolbar>
-          <Grid container
-          alignItems='center'
-          >
-            <Grid item xs={4} lg={4} className={classes.header2} >
-             <InputBase 
-             className={classes.input}
-               placeholder='Search with Nerus'
-               startAdornment={<SearchIcon fontSize='medium'/>}
-             />
-            </Grid>
-            <Grid item xs={8} lg={8} className={classes.header1} >
-             <IconButton>
-               <Badge badgeContent={6} color='secondary'>
-                 <NotficationNoneIcon/>
-               </Badge>
-            </IconButton>
-            <IconButton>
-               <Badge badgeContent={6} color='primary'>
-                <PowerSettingsNewIcon/>
-              </Badge>
-             </IconButton>
-             <IconButton >
-                <NotficationNoneIcon/>
-             </IconButton>
-            </Grid>
-          
+export default function Header() {
 
-          
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static" className={classes.root}>
+            <Toolbar>
+                <Grid container
+                    alignItems="center">
+                    <Grid item>
+                        <InputBase
+                            placeholder="Search topics"
+                            className={classes.searchInput}
+                            startAdornment={<SearchIcon fontSize="small" />}
+                        />
+                    </Grid>
+                    <Grid item sm></Grid>
+                    <Grid item>
+                        <IconButton>
+                            <Badge badgeContent={4} color="secondary">
+                                <NotificationsNoneIcon fontSize="small" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <Badge badgeContent={3} color="primary">
+                                <ChatBubbleOutlineIcon fontSize="small" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <PowerSettingsNewIcon fontSize="small" />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
 }
-
-export default Header
--- LoginAndRegistration/src/components/PageHeader.js
+++ LoginAndRegistration/src/components/PageHeader.js
@@ -1,47 +1,49 @@
-
-
-import { Card, Paper, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
 import React from 'react'
+import { Paper, Card, Typography, makeStyles, Button } from '@material-ui/core'
 
-const useStyles = makeStyles(theme=>({
-  root:{
-    background:'f4f5fd',
-  },
-  PageHeader:{
-    padding:'2rem',
-    marginBottom:'5rem',
-    display:'flex',
-  }
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: '#fdfdff'
+    },
+    pageHeader:{
+        padding:theme.spacing(4),
+        display:'flex',
+        marginBottom:theme.spacing(2)
+    },
+    pageIcon:{
+        display:'inline-block',
+        padding:theme.spacing(2),
+        color:'#3c44b1'
+    },
+    pageTitle:{
+        paddingLeft:theme.spacing(4),
+        '& .MuiTypography-subtitle2':{
+            opacity:'0.6'
+        }
+    }
 }))
 
-const PageHeader = (props) => {
-  const classes = useStyles()
-  const {Icon, title , subTitle} = props
-  return (
-    <Paper elevation={0} square className={classes.root}>
-      <div className={classes.pageHeader}>
-        <Card>
-          {Icon}
-        </Card>
-        <div className="">
-          <Typography 
-            variant='h5'
-            component="div"
-          >
-            {title}
-          </Typography>
+export default function PageHeader(props) {
 
-          <Typography 
-            variant='subtitle2'
-            component="div"
-          >
-            {subTitle}
-          </Typography>
-        </div>
-      </div>
-    </Paper>
-  )
+    const classes = useStyles();
+    const { title, subTitle, icon } = props;
+    return (
+        <Paper elevation={0} square className={classes.root}>
+            <div className={classes.pageHeader}>
+                <Card className={classes.pageIcon}>
+                    {icon}
+                </Card>
+                <div className={classes.pageTitle}>
+                    <Typography
+                        variant="h6"
+                        component="div">
+                        {title}</Typography>
+                    <Typography
+                        variant="subtitle2"
+                        component="div">
+                        {subTitle}</Typography>
+                </div>
+            </div>
+        </Paper>
+    )
 }
-
-export default PageHeader

b4118e4e08ba2342d27c8aa11de0b4369487df35
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-04 04:51:12 +0100

    experimentong with buttons

--- Restaurant/src/App.js
+++ Restaurant/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Button, makeStyles} from "@material-ui/core"
+import {Person} from "@material-ui/icons"
+
+const useStyles = makeStyles((theme)=>({
+  button:{
+    color:'#fff',
+    background:theme.palette.primary.main,
+  }
+}))
 
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello my world</h1>
+      </div>
+    </>
   );
 }
 
--- Restaurant/src/index.js
+++ Restaurant/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {theme} from './theme'
+import { ThemeProvider } from '@material-ui/styles';
 
 ReactDOM.render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
+    
+  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

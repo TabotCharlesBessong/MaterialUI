9fe7db89de2cd19614366bc3a2cb33022e5766df
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-13 14:47:38 +0100

    created the dataTable for single user

---
+++ adminDashboard/src/components/dataTable/DataTable.jsx
@@ -0,0 +1,58 @@
+
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+// import {DataGrid} from '@material-ui/core'
+import { userColumns, userRows } from "../../datatablesources";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const Datatable = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link to="/users/new" className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={data}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Datatable;
--- adminDashboard/src/pages/list/List.jsx
+++ adminDashboard/src/pages/list/List.jsx
@@ -1,11 +1,18 @@
 
 import React from 'react'
+import Datatable from '../../components/dataTable/DataTable'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './list.scss'
 
 const List = () => {
   return (
     <div className='list' >
-
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
     </div>
   )
 }

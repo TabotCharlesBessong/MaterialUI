3bdf9cf7d501b03b84b51105ff2fb31cd16938a3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-13 09:16:52 +0100

    done working on the chats components

--- adminDashboard/src/components/charts/Charts.jsx
+++ adminDashboard/src/components/charts/Charts.jsx
@@ -19,6 +19,7 @@ const Charts = () => {
     <div className="title">
       Last 6 month revenue
     </div>
+    <ResponsiveContainer width="100%" aspect={2 / 1} >
 <AreaChart width={730} height={250} data={data}
   margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
   <defs>
@@ -28,13 +29,14 @@ const Charts = () => {
     </linearGradient>
     
   </defs>
-  <XAxis dataKey="name" />
-  <YAxis />
-  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" stroke='gray' />
+  {/* <YAxis /> */}
+  <CartesianGrid strokeDasharray="3 3" className='chartGrid' />
   <Tooltip />
   <Area type="monotone" dataKey="total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
   
 </AreaChart>
+</ResponsiveContainer>
     </div>
   )
 }

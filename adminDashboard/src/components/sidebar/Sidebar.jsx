d1ab08898b580ac9991284fa99f932963e35cda7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-20 21:40:22 +0100

    made some change in the sidebar

--- adminDashboard/src/components/sidebar/Sidebar.jsx
+++ adminDashboard/src/components/sidebar/Sidebar.jsx
@@ -2,14 +2,18 @@
 import { Dashboard, DragHandle, ExitToApp, FlagTwoTone, GraphicEq, Healing, Notifications, PeopleAlt, Person, PinDropRounded, Settings } from '@material-ui/icons'
 import React from 'react'
 import './sidebar.scss'
+import {Link} from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className='sidebar' >
       <div className="top">
+      <Link to='/' style={{textDecoration:'none'}} >
         <span className="logo">
-          Ukujula
+            Ukujula
         </span>
+      </Link>
+        
       </div>
       <hr />
       <div className="center">

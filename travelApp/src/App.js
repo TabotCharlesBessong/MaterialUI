d635935905f5c5053d467fd854e476208649284e
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-04 11:11:42 +0100

    trying to start  a new projects with material ui

---
+++ travelApp/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Home from './pages/home/Home';
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import Login from './pages/login/Login';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import {userInputs , productInputs} from './formSource'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        {/* <Navbar/> */}
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home/>} />
+            <Route path='login' element={<Login/>} />
+            <Route path='users'>
+              <Route index element={<List/>} />
+              <Route path=':userId' element={<Single/>} />
+              <Route path='new' element={<New inputs={userInputs} title='Add a new user' />} />
+            </Route>
+            <Route path='products'>
+              <Route index element={<List/>} />
+              <Route path=':productId' element={<Single/>} />
+              <Route path='new' element={<New inputs={productInputs} title='Add a new products' />} />
+            </Route>
+          </Route>
+        </Routes>
+        {/* <Sidebar/> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;

c68785cd756defffa8dfda91cbe7e3373d54f979
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-06-21 15:15:09 +0100

    starting files of project manager

---
+++ project-manager/src/ui/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "./Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
+
+const useStyles = makeStyles(theme => ({
+  footer: {
+    backgroundColor: theme.palette.common.blue,
+    width: "100%",
+    zIndex: 1302,
+    position: "relative"
+  },
+  adornment: {
+    width: "25em",
+    verticalAlign: "bottom",
+    [theme.breakpoints.down("md")]: {
+      width: "21em"
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "15em"
+    }
+  },
+  mainContainer: {
+    position: "absolute"
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+    textDecoration: "none"
+  },
+  gridItem: {
+    margin: "3em"
+  },
+  icon: {
+    height: "4em",
+    width: "4em",
+    [theme.breakpoints.down("xs")]: {
+      height: "2.5em",
+      width: "2.5em"
+    }
+  },
+  socialContainer: {
+    position: "absolute",
+    marginTop: "-6em",
+    right: "1.5em",
+    [theme.breakpoints.down("xs")]: {
+      right: "0.6em"
+    }
+  }
+}));
+
+export default function Footer(props) {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      
+
+      <img
+        alt="black decorative slash"
+        src="static/assets/footerAdornment.svg"
+        className={classes.adornment}
+      />
+
+      
+    </footer>
+  );
+}

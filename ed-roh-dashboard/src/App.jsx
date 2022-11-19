456f7ac6ebb443ab6cb3621effbd46370b33fb24
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-19 04:56:59 +0100

    setting up

--- ed-roh-dashboard/src/App.jsx
+++ ed-roh-dashboard/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { Header } from './components'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello app</h1>
+    <div className="app">
+      <Header/>
     </div>
   )
 }
